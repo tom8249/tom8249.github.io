3afe5b6faaaf62c301b56aa0e2ffa4c5c1a4a767
tom8249 <yhy4651@gmail.com>
2021-08-08 23:25:18 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -3,8 +3,10 @@ const API_KEY = "309adc13ca0eb3436042b32fca4a7acf"
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-    console.log(url);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data => {
+        console.log(data.name,data.weather[0].main);
+    });
 }
 
 function onGeoError() {
