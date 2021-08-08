ee3f306be30a1a2fa6cf05b6cf6fe508cbfcc631
tom8249 <yhy4651@gmail.com>
2021-08-08 18:36:36 +0900

    Update clock.js

--- clock.js
+++ clock.js
@@ -2,7 +2,7 @@ const clock = document.querySelector("h2#clock");
 
 function KoreaTime() {
     const date = new Date();
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    clock.innerText = String(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`).padStart("2",0);
 }
 
 KoreaTime();
